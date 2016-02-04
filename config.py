7b9856785c416312844d883560d9abe81d941d70
Lucky <175917645@qq.com>
2016-02-05 00:45:28 +0800

    add openid for login page

--- config.py
+++ config.py
@@ -2,3 +2,12 @@
 # SECRET_KEY 配置仅仅当 CSRF 激活的时候才需要，它是用来建立一个加密的令牌，用于验证一个表单
 CSRF_ENABLED = True
 SECRET_KEY = 'love-love-love'
+
+# 定义一个 OpenID 提供者的列表
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }
+]
