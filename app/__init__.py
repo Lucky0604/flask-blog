fa01d747a3219783ff8fc0b3872c5811c0f9e63e
Lucky <175917645@qq.com>
2016-02-07 00:45:42 +0800

    rewrite login page use flask-openid, add real user login by yahoo id and password

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,26 @@
 
 __author__ = 'Lucky'
 
+import os
 from flask import Flask
 # 初始化应用程序的时候，我们也必须初始化数据库
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
+# For the login system we will use two extensions, Flask-Login and Flask-OpenID
+
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
 app = Flask(__name__)
-# read config.py
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+# read config.py
+
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -26,10 +26,12 @@ def index():
 '''
 
 # use template
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 # add login form
 from .forms import LoginForm
+from .models import User
 
 '''
 @app.route('/')
@@ -40,12 +42,22 @@ def index():
         title = 'Home',
         user = user)
 '''
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 # add some info
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Lucky'}        # fake user
+    # user = {'nickname': 'Lucky'}        # fake user
+    user = g.user           # real user
     posts = [
         {
         'author': {'nickname': 'John'},
@@ -62,16 +74,50 @@ def index():
         posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
+
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     # 验证并且存储表单数据
     if form.validate_on_submit():
-        flash('Login requested for OpenID = " ' + form.openid.data + ' " , remember_me = ' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
 
     return render_template('login.html',
         title = 'Sign In',
         form = form,
         # 登录视图函数中使用OpenID 提供者的列表
         providers = app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login, Please try again')
+        return redirect(url_for('login'))
+
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
