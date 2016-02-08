ac3e6846401ccc43479cf2b45605adce7b89dd8b
Lucky <175917645@qq.com>
2016-02-08 23:08:54 +0800

    setting up an email handle the exception info

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ import os
 from flask import Flask
 # 初始化应用程序的时候，我们也必须初始化数据库
 from flask.ext.sqlalchemy import SQLAlchemy
+# setting up an email when there is an exception is pretty easy
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 
 # For the login system we will use two extensions, Flask-Login and Flask-OpenID
@@ -24,6 +26,16 @@ lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 # read config.py
+
+if not app.config:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_hanlder = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_hanlder.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_hanlder)
 
 
 from app import views, models
--- config.py
+++ config.py
@@ -20,3 +20,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 # SQLALCHEMY_MIGRATE_REPO 是文件夹，我们将会把 SQLAlchemy-migrate 数据文件存储在这里
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['your@example.com']
