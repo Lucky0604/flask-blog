c1687b6471d0971eab5d1ac312f15bdc3463609e
Lucky <175917645@qq.com>
2016-02-08 22:52:33 +0800

    add 404 and 500 error pages

--- app/views.py
+++ app/views.py
@@ -159,3 +159,13 @@ def edit():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+# handle 404 error
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+# handle 500 error
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
